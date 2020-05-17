d7a76cd43a5ff124a4713d7318688af6842f7f2b
CJButton <gsyme1907@gmail.com>
2020-05-18 07:36:41 +0900

    feat: Nine

--- src/App.tsx
+++ src/App.tsx
@@ -67,6 +67,21 @@ const ClockIndex = () => {
             <header className="App-header">
                 <div className='digital-wrapper'>
 
+                <div className='digital-nine'>
+                    <div className='top-half'>
+                        <div className='top' />
+                        <div className='left-and-right'>
+                            <div className='left' />
+                            <div className='right' />
+                        </div>
+                    </div>
+                    <div className='center-top' />
+                    <div className='center-bottom' />
+                    <div className='bottom-half'>
+                        <div className='right' />
+                    </div>
+                </div>
+
                 <div className='digital-eight'>
                     <div className='top-half'>
                         <div className='top' />
