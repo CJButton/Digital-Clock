17003313b545995a3e91db74eec73403336f4599
CJButton <gsyme1907@gmail.com>
2020-05-24 14:55:10 +0900

    chore: Renaming of main component

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import DigitalClock from './DigitalClock';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DigitalClock />
   </React.StrictMode>,
   document.getElementById('root')
 );
